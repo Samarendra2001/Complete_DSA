bec503401c54051fdec96005dee8e6716211b791
Samrendra Barick <baricksamarendra@gmail.com>
2024-09-06 10:54:05 +0530

    Problem no 7 Solved

---
+++ Pattern_Problems/7.cpp
@@ -0,0 +1,17 @@
+//Numeric half pyramid
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int row =0;row<6;row++){
+        for(int col=0;col<row+1;col++){
+            cout<<col+1;
+        }
+        cout<<endl;
+    }
+}
